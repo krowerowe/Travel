f176ac8988716b5847383ae252cad5c72e024f30
krowerowe <krowerowe@users.noreply.github.com>
2025-07-25 16:47:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mapDiv = document.getElementById('world-map');
+    mapDiv.innerHTML = '<svg width="600" height="300" aria-hidden="true"><rect x="50" y="50" width="100" height="50" fill="orange"/><title>Greece</title></svg>';
+});
