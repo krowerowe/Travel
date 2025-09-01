b19acfda7a32132a2a988408bf78ebec0ddbc21d
krowerowe <krowerowe@users.noreply.github.com>
2025-08-31 22:26:24 -0500

    Create header.js

---
+++ js/header.js
@@ -0,0 +1,27 @@
+export const headerContent = `
+    <svg class="dotted-path" preserveAspectRatio="none" viewBox="0 0 100 100">
+        <path d="M 0,50 L 100,50" stroke="#d1d5db" stroke-width="1" stroke-dasharray="2,2" />
+    </svg>
+    <nav class="nav-list w-full max-w-7xl mx-auto">
+        <a href="#home" class="nav-item active" data-page="home">
+            <span class="nav-pin-container"><span class="nav-pin"></span></span>
+            <span class="dot"></span>
+            <span class="nav-link">Home</span>
+        </a>
+        <a href="#countries" class="nav-item" data-page="countries">
+            <span class="nav-pin-container"><span class="nav-pin"></span></span>
+            <span class="dot"></span>
+            <span class="nav-link">Countries</span>
+        </a>
+        <a href="#about" class="nav-item" data-page="about">
+            <span class="nav-pin-container"><span class="nav-pin"></span></span>
+            <span class="dot"></span>
+            <span class="nav-link">About</span>
+        </a>
+        <a href="#contact" class="nav-item" data-page="contact">
+            <span class="nav-pin-container"><span class="nav-pin"></span></span>
+            <span class="dot"></span>
+            <span class="nav-link">Contact</span>
+        </a>
+    </nav>
+`;
