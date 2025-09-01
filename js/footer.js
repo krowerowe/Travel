82866f170ce124caa397e82f854b3aa4a2c75cfc
krowerowe <krowerowe@users.noreply.github.com>
2025-08-31 22:47:27 -0500

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,5 @@
 export const footerContent = `
+<div class="bg-white rounded-t-3xl shadow-top mt-8 py-6 text-center text-gray-500 text-sm">
     <p>&copy; 2025 KROWE.NET. All rights reserved.</p>
+</div>
 `;
