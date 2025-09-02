1616e0d3a177fa84f5c2dc8b7bf7695197b1cce5
krowerowe <krowerowe@users.noreply.github.com>
2025-09-01 23:24:00 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ function drawPath() {
     const svgWidth = lastDotRect.right - firstDotRect.left;
     const svgHeight = navRect.height;
     
-    // Adjusted left position to correctly align with the first dot's container
+    // Position the SVG container to the exact left of the first dot's container
     pathSvg.style.width = `${svgWidth}px`;
     pathSvg.style.height = `${svgHeight}px`;
     pathSvg.style.left = `${firstDotRect.left - navRect.left}px`;
