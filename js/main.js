1763bce5752a8f3b1fe2da657f27d1fb687c8418
krowerowe <krowerowe@users.noreply.github.com>
2025-09-01 22:39:17 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -48,8 +48,8 @@ function drawPath() {
     // Adjusted left position to correctly align with the first dot's container
     pathSvg.style.width = `${svgWidth}px`;
     pathSvg.style.height = `${svgHeight}px`;
-    pathSvg.style.left = `${(firstDotRect.left - navList.getBoundingClientRect().left) + 12}px`;
-    pathSvg.style.top = `0px`;
+    pathSvg.style.left = `${firstDotRect.left}px`;
+    pathSvg.style.top = `${navRect.top}px`;
     pathSvg.setAttribute('viewBox', `0 0 ${svgWidth} ${svgHeight}`);
 
     const pathData = [];
